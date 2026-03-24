c2b511c807ecf4fc8bcd9736c03a4c5916cd3721
abdoukarim fall <abdoufallkarim@gmail.com>
2026-03-24 13:44:45 +0000

    Force HTTPS and trust proxies for Render deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -27,7 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
             \App\Http\Middleware\HandleInertiaRequests::class,
         ]);
 
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
