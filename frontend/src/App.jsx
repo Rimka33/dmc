44d7961b7206d65fbe629f94b9cc4ed01e0c7714
abdoukarim fall <abdoufallkarim@gmail.com>
2026-04-23 12:48:35 +0000

    Final cleanup: Fix frontend App.jsx structure and add missing NotificationProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { CartProvider } from './contexts/CartContext';
 import { WishlistProvider } from './contexts/WishlistContext';
+import { NotificationProvider } from './contexts/NotificationContext';
 import ScrollToTop from './components/ScrollToTop';
 
 import Home from './pages/Home';
@@ -28,37 +29,39 @@ import ForgotPassword from './pages/ForgotPassword';
 export default function App() {
     return (
         <AuthProvider>
-            <CartProvider>
-                <WishlistProvider>
-                    <BrowserRouter>
-                        <ScrollToTop />
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            <Route path="/shop" element={<Shop />} />
-                            <Route path="/produit/:id" element={<ProductDetail />} />
-                            <Route path="/contact" element={<Contact />} />
-                            <Route path="/blog" element={<Blog />} />
-                            <Route path="/blog/:slug" element={<BlogPost />} />
-                            <Route path="/panier" element={<Cart />} />
-                            <Route path="/wishlist" element={<Wishlist />} />
-                            <Route path="/checkout" element={<Checkout />} />
-                            <Route path="/checkout/received" element={<CheckoutReceived />} />
-                            <Route path="/mon-compte" element={<UserDashboard />} />
-                            <Route path="/mes-commandes" element={<Orders />} />
-                            <Route path="/connexion" element={<Auth defaultMode="login" />} />
-                            <Route path="/inscription" element={<Auth defaultMode="register" />} />
-                            <Route path="/forgot-password" element={<ForgotPassword />} />
-                            <Route path="/categorie/:slug" element={<Category />} />
-                            <Route path="/termes" element={<Terms />} />
-                            <Route path="/retours" element={<Returns />} />
-                            <Route path="/politique-de-confidentialite" element={<PrivacyPolicy />} />
-                            <Route path="/a-propos" element={<About />} />
-                            <Route path="/login" element={<Navigate to="/connexion" replace />} />
-                            <Route path="*" element={<Home />} />
-                        </Routes>
-                    </BrowserRouter>
-                </WishlistProvider>
-            </CartProvider>
+            <NotificationProvider>
+                <CartProvider>
+                    <WishlistProvider>
+                        <BrowserRouter>
+                            <ScrollToTop />
+                            <Routes>
+                                <Route path="/" element={<Home />} />
+                                <Route path="/shop" element={<Shop />} />
+                                <Route path="/produit/:id" element={<ProductDetail />} />
+                                <Route path="/contact" element={<Contact />} />
+                                <Route path="/blog" element={<Blog />} />
+                                <Route path="/blog/:slug" element={<BlogPost />} />
+                                <Route path="/panier" element={<Cart />} />
+                                <Route path="/wishlist" element={<Wishlist />} />
+                                <Route path="/checkout" element={<Checkout />} />
+                                <Route path="/checkout/received" element={<CheckoutReceived />} />
+                                <Route path="/mon-compte" element={<UserDashboard />} />
+                                <Route path="/mes-commandes" element={<Orders />} />
+                                <Route path="/connexion" element={<Auth defaultMode="login" />} />
+                                <Route path="/inscription" element={<Auth defaultMode="register" />} />
+                                <Route path="/forgot-password" element={<ForgotPassword />} />
+                                <Route path="/categorie/:slug" element={<Category />} />
+                                <Route path="/termes" element={<Terms />} />
+                                <Route path="/retours" element={<Returns />} />
+                                <Route path="/politique-de-confidentialite" element={<PrivacyPolicy />} />
+                                <Route path="/a-propos" element={<About />} />
+                                <Route path="/login" element={<Navigate to="/connexion" replace />} />
+                                <Route path="*" element={<Home />} />
+                            </Routes>
+                        </BrowserRouter>
+                    </WishlistProvider>
+                </CartProvider>
+            </NotificationProvider>
         </AuthProvider>
     );
 }
